773a1c271bed9e87ad072b91cdc3bb74895eb495
GoldFNX <ryanrobert2504@gmail.com>
2023-11-14 18:10:10 -0300

    feat: loading alert

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
@@ -20,46 +20,54 @@ import { Container } from "./styles";
 export default function Home() {
   const dispatch = useDispatch();
 
-  const {} = useQuery({
-    queryKey: ["apiProps"],
-    queryFn: async () => {
-      const { data } = await axios.get(
-        "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC"
-      );
-      dispatch({ type: ActionTypes.get, payload: data.products });
-    },
-  });
-
   const [cartIsOpen, setCartIsOpen] = useState(false);
   const { getData }: { getData: apiProps[] } = useSelector(
     (rootReducer: any) => rootReducer.getDataReducer
   );
 
+  const { data, isLoading } = useQuery({
+    queryKey: ["products-list"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC"
+      );
+      dispatch({ type: ActionTypes.get, payload: data.products });
+      return data.products as apiProps[];
+    },
+  });
+
   return (
     <>
-      <GlobalStyle />
-      <Header setCartIsOpen={setCartIsOpen} />
-      <Container>
-        <div className="card-box">
-          {getData.map((e) => {
-            return (
-              <CardItem
-                key={e.id}
-                id={e.id}
-                name={e.name}
-                brand={e.brand}
-                description={e.description}
-                photo={e.photo}
-                price={e.price}
-                createdAt={e.createdAt}
-                updatedAt={e.updatedAt}
-              />
-            );
-          })}
-        </div>
-      </Container>
-      <Footer />
-      {cartIsOpen && <MarketCart setCartIsOpen={setCartIsOpen} />}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <GlobalStyle />
+          <Header setCartIsOpen={setCartIsOpen} />
+
+          <Container>
+            <div className="card-box">
+              {getData.map((e) => {
+                return (
+                  <CardItem
+                    key={e.id}
+                    id={e.id}
+                    name={e.name}
+                    brand={e.brand}
+                    description={e.description}
+                    photo={e.photo}
+                    price={e.price}
+                    createdAt={e.createdAt}
+                    updatedAt={e.updatedAt}
+                  />
+                );
+              })}
+            </div>
+          </Container>
+          <Footer />
+          {cartIsOpen && <MarketCart setCartIsOpen={setCartIsOpen} />}
+        </>
+      )}
     </>
   );
 }
